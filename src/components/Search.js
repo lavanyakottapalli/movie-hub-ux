53318ca71524c53ac1aec245974ff0377b7fb6c0
lavanyakottapalli <146051923+lavanyakottapalli@users.noreply.github.com>
2023-09-30 11:54:20 -0700

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,7 +5,7 @@ const Search = (props) => {
         <div className='container-fluid'>
             <div className="input-group mt-5">
                 <div className='mx-auto input-large'>
-                    <input type="text" style={{ width: '600px', height: '50px' }}  class="cold-sm-5 form-control rounded-pill" placeholder="Search Movies.." aria-describedby="basic-addon1" value={props.value}
+                    <input type="text" style={{ width: '600px', height: '50px' }}  className="cold-sm-5 form-control rounded-pill" placeholder="Search Movies.." aria-describedby="basic-addon1" value={props.value}
                         onChange={(event) => props.setSearchValue(event.target.value)}>
                     </input>
                 </div>
