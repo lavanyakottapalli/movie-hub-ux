12173a45ceb08a8d514dc4e2e727995dba5fc2d2
lavanyakottapalli <lavanyakottapalli96@gmail.com>
2023-09-29 20:05:00 -0700

    Add react

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Movies from './components/Movies';
+import Search from './components/Search';
+import Header from './components/Header';
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState([]);
+
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://localhost:5008/movies`;
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ search: searchValue, searchFields: ['title', 'genres'], top: 30 })
+    };
+
+    const result = await fetch(url, requestOptions);
+    const json = await result.json();
+    if (json) {
+      setMovies(json);
+    }
+  };
+
+  useEffect(() => {
+      getMovieRequest(searchValue.length > 0 ? searchValue : "2023");
+  }, [searchValue]);
+
+  return (
+    <div className='body container-fluid'>
+          <Header />
+          <Search searchValue={searchValue} setSearchValue={setSearchValue} />
+          <Movies movies={movies} />
+    </div>
+  );
+};
+
+export default App;
